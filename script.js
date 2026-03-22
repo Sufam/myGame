46f2c7987b6ca682315c215ba5a5aa0d5071f2cf
SuRui <suben8n6f@gmail.com>
2026-03-23 00:03:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const container = document.getElementById('grid-container');
 for (let i = 0; i < 9; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid')
-    for (let i = 0; i < 9; i ++){
+    for (let j = 0; j < 9; j ++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
         grid.appendChild(cell);
