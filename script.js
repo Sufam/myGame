99b4893ce100cada65f0a7927e811b6f589c428e
SuRui <suben8n6f@gmail.com>
2026-03-24 00:01:21 +0800

    add back button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.getElementById('grid-container');
-let player = 1;
-let area = 5;
-let round = 0;
+let round = 1;
+let area = [];
+let cell_step = [];
 
 for (let i = 1; i < 10; i++) {
     const grid = document.createElement('div');
@@ -12,15 +12,15 @@ for (let i = 1; i < 10; i++) {
         cell.classList.add('cell');
 
         cell.addEventListener('click', function(){
-            if (i == area && cell.className == 'cell' || round == 0){
-                area = j;
-                round = 1;
-                if (player % 2 != 0){
+            if (i == area[area.length - 1] && cell.className == 'cell' || round == 1){
+                area.push(j);
+                if (round % 2 != 0){
                     this.classList.add('o');
                 }else{
                     this.classList.add('x');
                 }
-                player += 1;
+                cell_step.push(this);
+                round += 1;
             }
             
         });
@@ -28,4 +28,13 @@ for (let i = 1; i < 10; i++) {
         grid.appendChild(cell);
     }
     container.appendChild(grid);
+}
+
+function backstep(){
+    cell_step[cell_step.length - 1].className = "cell";
+    area.pop();
+    cell_step.pop();
+    if (round > 0){
+        round -= 1;
+    }
 }
